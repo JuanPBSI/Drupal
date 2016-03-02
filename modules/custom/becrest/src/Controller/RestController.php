95b4f3b646b644f92b93c4e2c5d2b9eeaae77975
Jose Juan Armenta <jose.armenta@bec.seguridad.unam.mx>
2016-03-02 07:24:59 -0600

    Modulo Back End

--- modules/custom/becrest/src/Controller/RestController.php
+++ modules/custom/becrest/src/Controller/RestController.php
@@ -27,8 +27,6 @@ class RestController extends ControllerBase {
   public function Rest()
   {
     $query = db_select('node_field_data', 'n');
-    #$query->join('taxonomy_index', 'ti', 'n.nid=ti.nid');
-    #$query->join('taxonomy_term_data', 't', 'ti.tid=t.tid');
     $query->fields('n', array('nid','title','created'))
           ->condition('status', 1) //publicados
           ->orderBy('created', 'DESC'); //Mas reciente primero
@@ -38,17 +36,72 @@ class RestController extends ControllerBase {
     while($registro = $result->fetchAssoc()) {
       $date=format_date($registro['created'],'custom','j M Y');
       $title=$registro['title'];
+      $idioma=$registro['langcode'];
       $articulos[$registro['nid']] = array(
         'title' => $title,
         'date' => $date
       );
     }
     return new JsonResponse($articulos);
-    #return [
-    #    '#type' => 'markup',
-    #    #'#markup' => $this->t('Implement method: Rest')
-    #    '#markup' => $this->t($ss)
-    #];
   }
 
+  public function Rest_comentarios($nodo)
+  {
+    $query = db_select('comment_field_data', 'cfd');
+    $query->join('comment__comment_body', 'ccb', 'cfd.cid=ccb.entity_id');
+    $query->fields('cfd', array('cid','created','status','subject','entity_id'))
+          ->fields('ccb', array('comment_body_value'))
+          ->condition('cfd.status', 1)
+          ->condition('cfd.entity_id', $nodo);
+    $result = $query->execute();
+    $articulos = array();
+    
+    while($registro = $result->fetchAssoc()) {
+      $date=format_date($registro['created'],'custom','j M Y');
+      $subject=$registro['subject'];
+      $nodo=$registro['entity_id'];
+      $content=$registro['comment_body_value'];
+      $articulos[$registro['cid']] = array(
+        'NodeId' => $nodo,
+        'subject' => $subject,
+        'date' => $date,
+        'contenido' => $content
+      );
+    }
+    return new JsonResponse($articulos);
+  }
+  
+  public function Rest_nodo($nodo)
+  {
+    $query = db_select('node_field_data', 'n');
+    $query->join('node__body', 'nb', 'n.nid=nb.entity_id');
+    $query->join('node__field_image', 'nfi', 'n.nid=nfi.entity_id');
+    $query->join('file_managed', 'fm', 'nfi.field_image_target_id=fm.fid');
+    $query->fields('n', array('nid','title','created','langcode'))
+          ->fields('nb', array('body_value'))
+          ->fields('fm', array('uri','filename'))
+          ->condition('n.status', 1)
+          ->condition('nid', $nodo)
+          ->orderBy('created', 'DESC');
+    $result = $query->execute();
+    $articulos = array();
+    
+    while($registro = $result->fetchAssoc()) {
+      $date=format_date($registro['created'],'custom','j M Y');
+      $title=$registro['title'];
+      $idioma=$registro['langcode'];
+      $content=$registro['body_value'];
+      $imagenSRC=$registro['uri'];
+      $imagenFile=$registro['filename'];
+      $articulos[$registro['nid']] = array(
+        'title' => $title,
+        'date' => $date,
+        'lang' => $idioma,
+        'contenido' => $content,
+        'img_src' => $imagenSRC,
+        'img_filename' => $imagenFile
+      );
+    }
+    return new JsonResponse($articulos);
+  }
 }
